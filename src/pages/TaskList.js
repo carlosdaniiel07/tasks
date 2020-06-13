b10d3e9b3428471537609f9261017f0ffb26bc1d
Carlos Daniel Martins de Almeida <carlosdaniiel0711@hotmail.com>
2020-06-13 18:03:16 -0300

    Deixando de esconder a tarefa quando ela é finalizada

--- src/pages/TaskList.js
+++ src/pages/TaskList.js
@@ -48,7 +48,7 @@ export default function TaskList() {
     };
 
     filterTasks();
-  }, [showDoneTasks, tasks]);
+  }, [showDoneTasks]);
 
   function toggleFilter() {
     setShowDoneTasks(!showDoneTasks);
